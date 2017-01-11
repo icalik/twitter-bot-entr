b60398002c3c29c45cca22ed34aae97a6c12be26
Ismet Said Calik <ismetsaid.calik@gmail.com>
2017-01-11 23:12:46 +0300

    1

---
+++ bot.js
@@ -0,0 +1,69 @@
+
+/////////////////////////////////////////
+//English - Turkish Twitter Bot
+//@author : Ismet Said Calik
+//@contact : ismetsaid.calik@gmail.com
+//@website : http://calik.me
+/////////////////////////////////////////
+
+console.log('English - Turkish Twiiter Bot');
+console.log('The bot is starting..');
+const loadJsonFile = require('load-json-file');
+
+var Twit = require('twit');
+var config = require('./config');
+
+var T = new Twit(config);
+var stream = T.stream('user');
+
+stream.on('follow',followed);
+
+tweetWord();
+
+setInterval(tweetWord,1000*10);
+
+function tweetWord() {
+	
+	loadJsonFile('./en-tr.json').then(json => {
+    select_id = Math.floor(Math.random() * (61196 - 1 + 1)) + 1;
+    
+    function selectWord(id) {
+      return (json[id-1].English + " : " + json[id-1].Turkish);
+    }
+    
+    k = selectWord(select_id);
+    tweetIt(k);
+    
+});
+}
+
+function followed(eventMsg) {
+	var name = eventMsg.source.name;
+	var screenName = eventMsg.source.screen_name;
+	var user_id = eventMsg.source.id;
+
+	tweetIt('@' + screenName + ' Takip ettiğin için teşekkürler, mobil bildirimleri açmayı unutma!');
+
+	follow_user(user_id);
+}
+
+function follow_user(user_id) {
+	T.post('friendships/create',{ user_id });
+	console.log("New Followers!");
+}
+
+function tweetIt(txt) {
+	var tweet = {
+	status : txt
+	
+	}
+	T.post('statuses/update',tweet,tweeted);
+	function tweeted(err,data,response) {
+		if (err) {
+			console.log("Something went wrong on tweet!");
+		}
+		else{
+			console.log("It Works!");
+		}
+	}
+}
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'wI0v0wx7soaIzUz8F2jButIV1',
+  consumer_secret:      'tFyZrWfTae1AKI4V1KG1WNaSnykEuLk0VRUdKanMSotvbxUnER',
+  access_token:         '2440700684-zJNrysUu0v7bd0Xh9L97WSPFYMVrUWk55LN8JEJ',
+  access_token_secret:  'dNRWYy69McBeaMwjhkEAaSEug3piwitNZJfPsWh8U11ps'
+
+}
